52b878ceafa59ad9db511ec49382161ccd2cdb49
johan duque <149849219+Johan1505duque@users.noreply.github.com>
2025-12-09 07:11:29 -0500

    The gitignore file is modified in order to add the dataconfig class without credentials to the repository

---
+++ src/main/java/com/curriculum/config/databaseConfig.java
@@ -0,0 +1,7 @@
+package com.curriculum.config;
+
+public class databaseConfig {
+    public static final String URL = "";
+    public static final String USER = "";
+    public static final String PASSWORD = "";
+}
