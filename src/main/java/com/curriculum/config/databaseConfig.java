90ee31348a99eb8106a364d34438a6e76ce99900
johan duque <149849219+Johan1505duque@users.noreply.github.com>
2025-12-09 07:48:53 -0500

    modification of the data config

--- src/main/java/com/curriculum/config/databaseConfig.java
+++ src/main/java/com/curriculum/config/databaseConfig.java
@@ -1,7 +1,14 @@
 package com.curriculum.config;
 
 public class databaseConfig {
-    public static final String URL = "";
-    public static final String USER = "";
-    public static final String PASSWORD = "";
+    public static String URL;
+    public static String USER;
+    public static String PASSWORD;
+
+    static {
+        URL = System.getenv("DATABASE_URL");
+        USER = System.getenv("DB_USER");
+        PASSWORD = System.getenv("DB_PASSWORD");
+    }
 }
+
